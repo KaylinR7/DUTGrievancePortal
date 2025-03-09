930aa059da1efc37e461eaf2c4f015b36edee99e
Samir Sunpersadh <samirsunpersadh53@gmail.com>
2025-03-09 19:16:40 +0200

    Added Tables to DB

--- DUTGrievancePortal/config.py
+++ DUTGrievancePortal/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///C:/Users/goven/Desktop/DUTGrievancePortal/DUTGrievancePortal.db'
+    SQLALCHEMY_DATABASE_URI = r'sqlite:///C:\Users\samir\source\repos\KaylinR7\DUTGrievancePortal\DUTGrievancePortal.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Removed all email-related configurations
