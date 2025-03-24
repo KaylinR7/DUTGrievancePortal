8d57f7e3dd208a266c039870d95910812e0d924d
KaylinR7 <govenderkaylin2913@gmail.com>
2025-03-24 22:09:53 +0200

    Update run.py

--- run.py
+++ run.py
@@ -7,8 +7,8 @@ db = SQLAlchemy()
 
 app = Flask(__name__)
 
-# Set up database URI from environment variable
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('postgresql://dutgreivanceportal_user:bxnLojOc7vZDSnS6h1mDPDwcUK0v0IFZ@dpg-cvgr7h8fnakc73elosj0-a.oregon-postgres.render.com/dutgreivanceportal')
+# Set up database URI directly (you don't need os.getenv() here if the URI is a static string)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dutgreivanceportal_user:bxnLojOc7vZDSnS6h1mDPDwcUK0v0IFZ@dpg-cvgr7h8fnakc73elosj0-a.oregon-postgres.render.com/dutgreivanceportal'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize the database with the app
