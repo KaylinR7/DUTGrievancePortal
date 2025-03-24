cf39c3f7508f0480756dffb645eefbca72e06d88
KaylinR7 <govenderkaylin2913@gmail.com>
2025-03-24 21:31:45 +0200

    Update config.py

--- DUTGrievancePortal/config.py
+++ DUTGrievancePortal/config.py
@@ -2,8 +2,9 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = "sqlite:////Users/nuveshannaicker/Desktop/DUTGrievancePortal/DUTGrievancePortal.db"
-
-
-
+    
+    # Store the database inside the project directory
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'database.db')}"
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
