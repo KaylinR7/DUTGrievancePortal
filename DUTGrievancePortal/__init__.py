a9bc4f71e720d8c0497cbe266cae7736f527b09c
KaylinR7 <govenderkaylin2913@gmail.com>
2025-03-24 23:08:23 +0200

    Update __init__.py

--- DUTGrievancePortal/__init__.py
+++ DUTGrievancePortal/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from config import Config  # Absolute import from root
+from config import Config
+from .extensions import db, login_manager
 
 def create_app(config_class=Config):
     app = Flask(__name__, template_folder="templates")
     app.config.from_object(config_class)
     
     # Initialize extensions
-    from .extensions import db, login_manager
     db.init_app(app)
     login_manager.init_app(app)
     
