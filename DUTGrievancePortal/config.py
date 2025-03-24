23deef5c4eee4597a42dcb780faddfe5f889f262
KaylinR7 <govenderkaylin2913@gmail.com>
2025-03-24 21:26:35 +0200

    Update config.py

--- DUTGrievancePortal/config.py
+++ DUTGrievancePortal/config.py
@@ -2,6 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = r'sqlite:///C:\Users\samir\source\repos\KaylinR7\DUTGrievancePortal\DUTGrievancePortal.db'
+    SQLALCHEMY_DATABASE_URI = "sqlite:////Users/nuveshannaicker/Desktop/DUTGrievancePortal/DUTGrievancePortal.db"
+
+
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # Removed all email-related configurations
