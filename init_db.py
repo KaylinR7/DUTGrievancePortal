d3a5ec72675a037c185faad9d2c394e8213852c2
KaylinR7 <govenderkaylin2913@gmail.com>
2025-11-20 13:08:54 +0200

    Delete init_db.py

--- init_db.py
+++
@@ -1,8 +0,0 @@
-﻿from DUTGrievancePortal import create_app, db
-from DUTGrievancePortal.models import Complaint, User  # Add User model
-
-app = create_app()
-
-with app.app_context():
-    db.create_all()
-    print("✅ Database tables created!")
